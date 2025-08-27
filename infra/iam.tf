6c242ddca12f82ad375ff44a27f267fa3d03b869
sbd <sebastianbd95@gmail.com>
2025-08-27 14:01:52 -0500

    fix workflow

--- infra/iam.tf
+++ infra/iam.tf
@@ -26,6 +26,11 @@ resource "aws_iam_role_policy_attachment" "ecr_ro" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "s3_full" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${local.name}-ec2-profile"
   role = aws_iam_role.ec2_role.name
