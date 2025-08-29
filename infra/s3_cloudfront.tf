ef223c587c6484d497cd2ba3f59f7bd53b2b1a59
sbd <sebastianbd95@gmail.com>
2025-08-29 13:36:59 -0500

    fix front deployment

--- infra/s3_cloudfront.tf
+++ infra/s3_cloudfront.tf
@@ -85,3 +85,4 @@ resource "aws_s3_bucket_policy" "frontend" {
 
 output "frontend_bucket" { value = aws_s3_bucket.frontend.bucket }
 output "cloudfront_domain" { value = aws_cloudfront_distribution.cdn.domain_name }
+output "cloudfront_distribution_id" { value = aws_cloudfront_distribution.cdn.id }
